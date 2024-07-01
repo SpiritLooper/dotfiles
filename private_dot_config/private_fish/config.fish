1d1c7a8652b54913978de4df33bf09adbe0bf5e1
Julien CLAISSE <julienclaisse@orange.fr>
2024-07-01 17:49:49 +0200

     Configure Home-manager to handle user packages

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,5 +6,8 @@ end
 # Disable greetings
 set fish_greeting
 
+# sourcing nix envs
+babelfish < $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh | source 
+
 # Enable starship 
 starship init fish | source
