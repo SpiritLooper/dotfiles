e87d6026a605342b9851d9c3b8f1a5c7135ac671
SpiritLooper <julienclaisse@orange.fr>
2024-07-08 16:12:58 +0200

    󰻈 Add kubernetes conf
    - Command Alias
    - kctx, kns
    - context prompt

--- private_dot_config/private_fish/conf.d/alias.fish
+++ private_dot_config/private_fish/conf.d/alias.fish
@@ -4,6 +4,11 @@ alias cat='bat --paging=never'
 # LS alias
 alias ls='lsd'
 
+# Kubernetes
+alias k 'kubectl'
+alias kctx 'kubectx'
+alias kns 'kubens'
+
 # Git diff
 function batdiff --description "Better Git diff with batcat"
     git diff --name-only --relative --diff-filter=d | xargs bat --diff
