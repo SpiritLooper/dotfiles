2ff0ae1f7c9d36920b1ef034bc57929aad86575b
Julien CLAISSE <julienclaisse@orange.fr>
2024-10-22 17:14:07 +0200

    🕑 Add clock-tty + 'clock' alias

--- private_dot_config/private_fish/conf.d/alias.fish
+++ private_dot_config/private_fish/conf.d/alias.fish
@@ -9,6 +9,9 @@ alias k 'kubectl'
 alias kctx 'kubectx'
 alias kns 'kubens'
 
+# TTY Clock
+alias clock 'tty-clock -sxcbD -d 0.99s'
+
 # Git diff
 function batdiff --description "Better Git diff with batcat"
     git diff --name-only --relative --diff-filter=d | xargs bat --diff
